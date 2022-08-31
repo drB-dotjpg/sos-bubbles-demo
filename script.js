e5673327d048833575db733ff1de04e538b20431
Derek Bond <derek.bond24@gmail.com>
2022-08-31 00:23:57 -0500

    added overlay concept images

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const numBubbles = 36;
 const bubbleTemplate = '<div class="bubble"><svg width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg"><circle class="circle-svg" cx="40" cy="40" r="40" fill="#6E9AA3" fill-opacity="0.25""/></svg></div>'
 const pageWrapper = document.querySelector(".page-wrapper");
 
-pageWrapper.innerHTML = bubbleTemplate.repeat(numBubbles);
+pageWrapper.innerHTML = pageWrapper.innerHTML + bubbleTemplate.repeat(numBubbles);
 
 const bubbles = document.querySelectorAll(".bubble");
 
